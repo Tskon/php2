7dcf3fae4328431de8b83b7d7a32ba45400e81c5
tskon <tsekhmeister.k@gmail.com>
2019-10-26 22:21:46 +0300

    create subclass

--- lesson1/PriceTag.php
+++ lesson1/PriceTag.php
@@ -1,18 +1,27 @@
 <?php
 
 class PriceTag {
-  private $name;
-  private $amount;
-  private $id;
+  public $name;
+  public $amount;
+  public $id;
 
-  public function __construct($name, $amount) {
+  public function __construct(string $name, int $amount) {
     $this->name = $name;
     $this->amount = $amount;
     $this->id = rand(1, 1000);
   }
 
   public function showInfo() {
-    print ($this->id.' '.$this->name.': '.$this->amount."$<br>");
+    print ($this->id . ' ' . $this->name . ': ' . $this->amount . "$<br>");
+  }
+}
 
+class PriceTagRubles extends PriceTag {
+  public function __construct(string $name, int $amount) {
+    parent::__construct($name, $amount);
+  }
+
+  public function showInfo() {
+    print ($this->id . ' ' . $this->name . ': ' . $this->amount . "руб.<br>");
   }
 }
--- lesson1/index.php
+++ lesson1/index.php
@@ -3,6 +3,8 @@ include 'PriceTag.php';
 
 $orangePriceTag = new PriceTag('orange', 20);
 $applePriceTag = new PriceTag('apple', 10);
+$applePriceTagRus = new PriceTagRubles('apple', 700);
 
 $orangePriceTag -> showInfo();
 $applePriceTag -> showInfo();
+$applePriceTagRus -> showInfo();
