d8e264d268e91a1e14660b3560f2838105fc6ab4
tskon <tsekhmeister.k@gmail.com>
2019-11-03 13:39:57 +0300

    twig installed

--- lesson3/index.php
+++ lesson3/index.php
@@ -1,1 +1,13 @@
 <?php
+require_once '../vendor/autoload.php';
+$loader = new \Twig\Loader\ArrayLoader([
+  'index' => 'Hello {{ name }}!',
+]);
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index', ['name' => 'Fabien']);
+
+$dir = "./images/";
+$imageList = array_diff(scandir($dir), ['..', '.']);
+
+var_dump($imageList);
