c2ad67c06918babdfbbff4f54b4438a112307d07
tskon <tsekhmeister.k@gmail.com>
2019-11-11 23:44:16 +0300

    setup index file for lesson 4

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
-$title = 'php2';
-$text = 'Test php 7.3 from ubuntu server';
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $title ?></title>
-</head>
-<body>
-<h1><?= $text ?></h1>
-</body>
-</html>
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include 'lesson4/index.php';
